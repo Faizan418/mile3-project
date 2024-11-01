54e0185bf1bd4fcd0a0fb279228dc06d80741dfa
Muhammad Faizan Soomro <141871174+Faizan418@users.noreply.github.com>
2024-11-02 01:11:27 +0500

    update

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -4,46 +4,45 @@ import Image from 'next/image'
 
 function Hero() {
   return (
-    <div className={styles.containar}>
+    <div className={styles.container}>
 
         <div className={styles.one}>
             <h1 className={styles.head_one}>New Arrivals</h1>
         </div>
+        
         <div className={styles.two}>
-
-            <div className={styles.div_one}>
+            <div className={styles.card}>
                 <Image src={'/images/bicycle-1.jpg'} alt='one' height={180} width={270}/>
                 <p>Bicycles</p>
                 <h2>Kryo X26 MTB--Model K</h2>
                 <p>--------</p>
                 <h1>$350.00</h1>
             </div>
-            <div className={styles.div_two}>
+            <div className={styles.card}>
                 <Image src={'/images/bicycle-2.jpg'} alt='two' height={180} width={270}/>
                 <p>Bicycles</p>
                 <h2>Kryo X26 MTB--Model X</h2>
                 <p>--------</p>
                 <h1>$350.00</h1>
             </div>
-            <div className={styles.div_three}>
+            <div className={styles.card}>
                 <Image src={'/images/bicycle-3.jpg'} alt='three' height={180} width={270}/>
                 <p>Bicycles</p>
                 <h2>Kryo X26 MTB--Model Y</h2>
                 <p>--------</p>
                 <h1>$350.00</h1>
             </div>
-            <div className={styles.div_four}>
+            <div className={styles.card}>
                 <Image src={'/images/bicycle-4.jpg'} alt='four' height={180} width={270}/>
                 <p>Bicycles</p>
                 <h2>Kryo X26 MTB--Model Z</h2>
                 <p>--------</p>
                 <h1>$350.00</h1>
             </div>
-
         </div>
       
     </div>
   )
 }
 
-export default Hero
+export default Hero;
